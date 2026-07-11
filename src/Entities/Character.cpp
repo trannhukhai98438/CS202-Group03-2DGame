09163617f54c9b2ae1e7ff05ecad51d833b8f5e8
Nhu Khai <trannhukhai98438@gmail.com>
2026-07-11 16:53:42 +0700

    fix: Change from hp logic (int) to state logic

--- src/Entities/Character.cpp
+++ src/Entities/Character.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include "Entities/Character.h"
 
 Character::Character(float x, float y)
@@ -41,4 +42,27 @@ bool Character::getIsAlive() const {
 
 bool Character::isDead() const {
     return !isAlive;
+=======
+#include "../../include/Entities/Character.h"
+
+Character::Character(float x, float y){
+    position={x,y};
+    velocity={0.f, 0.f};
+    facingRight=true;
+    isGrounded=false;
+    isActive=true;
+}
+
+void Character::die()
+{
+    isActive=false;
+}
+
+sf::FloatRect Character::getBounds(){
+    return hitbox.getGlobalBounds();
+}
+
+bool Character::isDead() const{
+    return (!isActive);
+>>>>>>> 40981ce (fix: Change from hp logic (int) to state logic)
 }
